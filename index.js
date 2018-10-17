3d09c938a6515c00f140d3f00335c994d934b48c
Dheeraj Mohan <dheeraj.98@outlook.com>
2018-10-17 23:05:39 +0530

    Implement fetching for past 8 days asynchronously. 	modified:   .gitignore 	modified:   index.js 	modified:   package.json 	modified:   src/apixu.js 	deleted:    src/temperature.json 	deleted:    temperature.json

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
-const apixu = require('./src/apixu');
+const weather = require('./src/apixu');
 const fs = require('fs');
+const moment = require('moment');
 
 var app = express();
+var weatherFetcher = weather();
 const port = 3000;
+
+function log_to_csv(data) {
+    data['month']=moment().format('M')-1;
+    fs.writeFile('./data.json', JSON.stringify(data, null, 4), function (err) {
+        if (err) {
+            console.log("Error writing to file");
+            return
+        }
+        console.log("Successfully saved data");
+    })
+}
 
 app.get('/', function (req, res) {
     res.send("Hello World!");
@@ -12,16 +25,8 @@ app.get('/', function (req, res) {
 app.get('/heatwave', function (req, res) {
     console.log(req.query.lat);
     console.log(req.query.lon);
-    jsonObj = apixu.getForecastData(req.query.lat, req.query.lon, function (data) {
-        res.json(data);
-        fs.writeFile('./temperature.json', JSON.stringify(data, null, 4), function (err) {
-            if (err) {
-                console.log("Error writing to file");
-                return
-            }
-            console.log("Successfully saved data");
-        })
-    });
+    weatherFetcher.getForecastData(req.query.lat, req.query.lon, log_to_csv);
+    weatherFetcher.getPriorData(req.query.lat, req.query.lon, log_to_csv);
 });
 
 app.listen(process.env.PORT || port);
--- src/apixu.js
+++ src/apixu.js
@@ -1,19 +1,70 @@
 const request = require('request');
-exports.getForecastData = function (lat, long, callback) {
-    var location = lat + "," + long;
+const moment = require('moment');
+function WeatherFetch(config) {
+    this.config = config;
+    this.data = { forecast: [], prior: [] };
+    this.forecastFlag = false;
+    this.priorFlag = false;
+}
+
+WeatherFetch.prototype.getForecastData = function (lat, long, callback) {
+    const location = lat + "," + long;
     const requestUri = "http://api.apixu.com/v1/forecast.json?key=a587248a962148a184b125410181610&q=" + location + "&days=5";
     var parsed = {};
+
     request({
         uri: requestUri,
         method: "GET",
         followRedirect: true,
-    }, function (error, response, body) {
-        parsed = JSON.parse(body);
-        forecastday = parsed.forecast.forecastday;
-        data = {data:[]};
-        for(let i =0;i<forecastday.length;i++){
-            data.data.push({'max_temp':forecastday[i]['day']['maxtemp_c'],'min_temp':forecastday[i]['day']['mintemp_c']})
+    }, (error, response, body) => {
+        if(error){
+            console.log('Error in api');
         }
-        callback(data);
+        parsed = JSON.parse(body);
+        var forecastday = parsed.forecast.forecastday;
+
+        for (var i = 0; i < forecastday.length; i++) {
+            this.data.forecast.push({ 'max_temp': forecastday[i]['day']['maxtemp_c'], 'min_temp': forecastday[i]['day']['mintemp_c'] });
+        }
+        // callback(data);
+        this.forecastFlag = true;
+        if (this.forecastFlag && this.priorFlag) {
+            callback(this.data);
+        }
     });
 }
+
+WeatherFetch.prototype.getPriorData = function (lat, long, callback) {
+    const location = lat + "," + long;
+    const requestUri = "http://api.apixu.com/v1/history.json?key=a587248a962148a184b125410181610&q=" + location + "&dt=";
+    var counter = 0;
+    var priorNumDays = 8;
+    for (var i = 0; i < priorNumDays; i++) {
+        request({
+            uri: requestUri + moment().subtract(i, 'days').format('YYYY-MM-DD'),
+            method: "GET",
+            followRedirect: true,
+        }, (error, response, body) => {
+            if(error){
+                console.log('Error in api');
+            }
+            var parsed = JSON.parse(body);
+            var forecastday = parsed.forecast.forecastday;
+            this.data.prior.push({ 'max_temp': forecastday[0]['day']['maxtemp_c'], 'min_temp': forecastday[0]['day']['mintemp_c'] });
+            counter = counter + 1;
+            if (counter == priorNumDays) {
+                this.priorFlag = true;
+            }
+            if (this.forecastFlag && this.priorFlag) {
+                callback(this.data);
+            }
+        });
+    }
+
+}
+
+function weather(config) {
+    return new WeatherFetch(config);
+}
+
+module.exports = weather;
