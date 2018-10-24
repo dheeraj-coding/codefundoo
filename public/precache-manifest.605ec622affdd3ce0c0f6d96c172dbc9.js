68badb433757167a30ad5530958ee59dcf194559
Dheeraj Mohan <dheeraj.98@outlook.com>
2018-10-24 19:12:23 +0530

    Add built dashboard files and integrated with the main app  Changes to be committed: 	new file:   public/asset-manifest.json 	new file:   public/dashboard.html 	new file:   public/favicon.ico 	modified:   public/index.html 	new file:   public/manifest.json 	new file:   public/precache-manifest.605ec622affdd3ce0c0f6d96c172dbc9.js 	new file:   public/service-worker.js 	new file:   public/static/css/main.1def8b0a.chunk.css 	new file:   public/static/css/main.1def8b0a.chunk.css.map 	new file:   public/static/js/1.73ee94cb.chunk.js 	new file:   public/static/js/1.73ee94cb.chunk.js.map 	new file:   public/static/js/main.688a5201.chunk.js 	new file:   public/static/js/main.688a5201.chunk.js.map 	new file:   public/static/js/runtime~main.229c360f.js 	new file:   public/static/js/runtime~main.229c360f.js.map

---
+++ public/precache-manifest.605ec622affdd3ce0c0f6d96c172dbc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "688a5201665053c18a24",
+    "url": "/static/js/main.688a5201.chunk.js"
+  },
+  {
+    "revision": "73ee94cb44d1cce5942e",
+    "url": "/static/js/1.73ee94cb.chunk.js"
+  },
+  {
+    "revision": "688a5201665053c18a24",
+    "url": "/static/css/main.1def8b0a.chunk.css"
+  },
+  {
+    "revision": "a188ae665596968eabe70a0f6e5266a4",
+    "url": "/index.html"
+  }
+];
